d49dbbfcc998de1c6ca1e72fefaaf7af61a1d564
Stephan Bollauf <stephan.bollauf@gmail.com>
2024-05-21 14:01:40 +0200

    add AccommodationType to questionaire

---
+++ client/src/data/accommodationTypes.js
@@ -0,0 +1,8 @@
+const accommodationTypes = {
+    hotel: "hotel",
+    hostel: "hostel",
+    bedAndBreakfast: "bed and breakfast",
+    vacationHomes: "vacation homes",
+}
+
+export default accommodationTypes
