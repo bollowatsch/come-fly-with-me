1a5e0dc6f0e586c1429e7ff43985f16997caf0c8
Nico <nico.orth@stud.fh-campuswien.ac.at>
2024-05-31 21:57:57 +0200

    created new apiRouter to handle api requests from frontend in the future. implemented weather & hotel search API's & endpoints.

---
+++ server/api/weather.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const keys = require('../apiKeys')
+require('dotenv').config();
+const API_KEY = process.env.API_KEY ? process.env.API_KEY : keys.WEATHER_KEY;
+
+module.exports.getWeather = async function getWeather(city) {
+    const options = {
+        method: 'GET',
+        url: 'https://yahoo-weather5.p.rapidapi.com/weather',
+        params: {
+            location: city,
+            format: 'json',
+            u: 'c'
+        },
+        headers: {
+            'X-RapidAPI-Key': API_KEY,
+            'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        return response.data
+    } catch (error) {
+        console.error(error);
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,10 @@
-var express = require('express');
-var router = express.Router();
-const bookingApi = require('../api/bookingApi');
+const express = require('express');
+const router = express.Router();
+const bookingApi = require('../api/booking');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
-
-router.get('/api', async function (req, res) {
-  res.send(await bookingApi.get());
-})
 
 module.exports = router;
